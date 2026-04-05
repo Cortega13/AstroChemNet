1860aff658448ed799275ebfabaf3705a4183c2d
Carlos Ortega <carlosfortega2001@gmail.com>
2026-04-05 23:56:25 +0000

    updates

--- run_tacc.sh
+++ run_tacc.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-#SBATCH -J training                          # Job name
+#SBATCH -J astrochem-surr-train              # Job name
 #SBATCH -o log.%j                         	 # Name of stdout output file (%j expands to jobId)
 #SBATCH -p gh-dev                            # Queue name
 #SBATCH -N 1                                 # Total number of nodes requested (56 cores/node)
 #SBATCH -n 1                                 # Total number of mpi tasks requested
-#SBATCH -t 2:00:00                           # Run time (hh:mm:ss)
+#SBATCH -t 0:30:00                           # Run time (hh:mm:ss)
 #SBATCH -A AST24021							 # Project charge code
 
-# Load CUDA module
-module load cuda/12.8
-
-# Load tacc-surrogates libraries
-source /scratch/py-envs/tacc-surrogates/bin/activate
-export PYTHONPATH=$SCRATCH/scripts/tacc-surrogates
-
-# Run flow bench training/evaluation
-python -W ignore epidemic_transfer_learning.py
-#python -W ignore model_analysis.py
+module load gcc
+module load python3_mpi/3.11.8
+source /work/09338/carlos9/vista/AstroChemNet/.venv/bin/activate
+python run.py train latent_ode --dataset uclchem_grav
